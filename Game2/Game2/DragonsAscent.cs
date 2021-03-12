b7788eaa8ac3337b575ff86c7cab21c164ebb73d
Thomas Reece <tommyr11@ksu.edu>
2021-03-12 12:53:51 -0600

    StateManagementFolder

--- Game2/Game2/DragonsAscent.cs
+++ Game2/Game2/DragonsAscent.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using System.Threading;
+using Game2.StateManagement;
 
 namespace Game2
 {
