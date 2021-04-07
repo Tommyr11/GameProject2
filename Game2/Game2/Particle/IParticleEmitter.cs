ea4f8dc35028202df040fa2c1ded974c70edbfbc
Thomas Reece <tommyr11@ksu.edu>
2021-04-07 14:36:38 -0500

    Particle Generic Classes added

---
+++ Game2/Game2/Particle/IParticleEmitter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Game2.Particle
+{
+    interface IParticleEmitter
+    {
+        public Vector2 Position { get; }
+
+        public Vector2 Velocity { get; }
+    }
+}
